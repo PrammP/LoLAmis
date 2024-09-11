83c53e7fece8e7db66d341d508d6976cbe8a39e7
PrammP <92103872+PrammP@users.noreply.github.com>
2024-09-11 16:48:09 +0200

    env

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 const db = require("./db");
 
 const app = express();
-const port = 5500;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
